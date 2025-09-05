15f32c09375f648d30d601fb2e57c07401f5ae6a
ccsanei <charly@thesanei.com>
2025-09-05 16:53:01 +0200

    Add files via upload

---
+++ 20250904132425-MUBQ2BEN.js
@@ -0,0 +1,33 @@
+window.botpress.init({
+  "botId": "5928e0b0-d5f6-448b-a59f-f61648cb5b90",
+  "configuration": {
+    "version": "v2",
+    "composerPlaceholder": "Écrivez votre message...",
+    "botName": "Les Planards",
+    "botAvatar": "https://files.bpcontent.cloud/2025/09/04/13/20250904133140-BFTRY30I.png",
+    "fabImage": "https://files.bpcontent.cloud/2025/09/05/14/20250905143028-D6S3WV4P.jpeg",
+    "website": {
+      "title": "https://www.planards.fr/",
+      "link": "https://www.planards.fr/"
+    },
+    "email": {
+      "title": "info@planards.fr",
+      "link": "info@planards.fr"
+    },
+    "phone": {
+      "title": "+33 4 50 53 08 97",
+      "link": "+33 4 50 53 08 97"
+    },
+    "termsOfService": {},
+    "privacyPolicy": {},
+    "color": "#b8161d",
+    "variant": "solid",
+    "headerVariant": "solid",
+    "themeMode": "light",
+    "fontFamily": "inter",
+    "radius": 4,
+    "feedbackEnabled": false,
+    "footer": "[⚡ by The Sane Intention](https://fr.linkedin.com/company/the-sane-intention)"
+  },
+  "clientId": "9eabbf82-2bdf-4274-968a-320952bc51cd"
+});
